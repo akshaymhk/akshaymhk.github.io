d2a7a6ee03dfb84dda0559653dc1f207fa368192
akshaymhk <akshay.mishra@brightlifecare.com>
2018-12-20 14:14:42 +0530

    truecaller test
    Signed-off-by: akshaymhk <akshay.mishra@brightlifecare.com>

---
+++ main.js
@@ -0,0 +1,17 @@
+function openDeepLink() {
+    console.log("CHECKING TRUECALLER INSTALLED OR NOT");
+    var wnd = window.open("truecallersdk://truesdk/web_verify?requestNonce=1234567890&partnerKey=vq25E27e4ed092bc646d3aefe1e3fc4810248&partnerName=AKMTC&lang=en&title=login");
+    setTimeout(function () {
+        if (wnd.location.href === 'about:blank') {
+            // Truecaller app not present on the device and you redirect the user 
+            // to your alternate verification page
+            wnd.close();
+            alert("TRUECALLER NOT INSTALLED");
+        } else {
+            // Truecaller app present on the device and the profile overlay opens
+            // The user clicks on verify and you'll receive the user's access token to fetch the profile on 
+            // your callback URL - post which, you can refresh the session at your frontend and complete the user verification
+            alert("TRUECALLER INSTALLED");
+        }
+    }, 200)
+}
